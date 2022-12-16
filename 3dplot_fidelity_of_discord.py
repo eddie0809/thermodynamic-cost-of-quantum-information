2e4ceafff140c224b34a4639de2bf24893d8b91e
Etienne Springer <esp8999@gmail.com>
2022-12-16 16:05:49 +0100

    two qubit fidelity

--- 3dplot_fidelity_of_discord.py
+++ 3dplot_fidelity_of_discord.py
@@ -10,23 +10,32 @@ plt.rcParams['text.latex.preamble'] = r'\usepackage[utf8]{inputenc}\usepackage[T
 mpl.rcParams['font.family'] = ['serif']
 
 cmap = mpl.cm.get_cmap('viridis')
+cmap_2 = mpl.cm.get_cmap('plasma')
+
 
 npzfile = np.load("fid_of_discord.npz")
+npzfile_both = np.load("two_fid_of_discord.npz")
 
-fid_of_t = npzfile['fid_of_t']
-discord = npzfile['discord']
+fid_of_t_local = npzfile['fid_of_t']
+discord_local = npzfile['discord']
+
+fid_of_t_both_qubits = npzfile_both['fid_of_t']
+discord_both_qubits = npzfile_both['discord']
 
 t_stop = np.pi
 dt = 1e-3
 t = np.arange(0, t_stop, dt)
 
 fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
-for ind, discord_value in enumerate(discord):
-    ax.plot(t, fid_of_t[:,ind], '.', markersize=.5, zs=discord_value, zdir='y', color=cmap(discord_value/max(discord)))
+for ind, discord_value in enumerate(discord_local):
+    ax.plot(t, fid_of_t_local[:,ind], '.', markersize=.5, zorder=ind, alpha=.6, zs=discord_value, zdir='y', color=cmap(discord_value/max(discord_local)))
+    
+for ind, discord_value in enumerate(discord_both_qubits):
+    ax.plot(t, fid_of_t_both_qubits[:,ind], '.', markersize=.5, zorder=ind, zs=discord_value, zdir='y', color=cmap_2(discord_value/max(discord_both_qubits)))
 ax.set_xlim(0,3.2)
-ax.set_zlim(1/np.sqrt(2),1)
+ax.set_zlim(1/2,1)
 ax.set_xlabel(r'$t$')
 ax.set_ylabel(r'$D(\alpha)$')
 ax.set_zlabel(r'Fidelity')
-ax.view_init(elev=25., azim=-90)
+ax.view_init(elev=0., azim=270)
 plt.show()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 from generate_states import generate_hamiltonian
-
+from qutip import *
 import xy_chain
 
 
@@ -11,10 +11,12 @@ beta = [1/300, 1/300]
 N = 5
 Hint = generate_hamiltonian(N)
 
-poss_alpha = [i/100 for i in range(101)]
+poss_alpha = [i/12 for i in range(13)]
 
 for ind, reduced_alpha in enumerate(poss_alpha):
 	system = xy_chain.System(t, N, Hint, reduced_alpha, beta)
 	system.calculate_quantities()
-	xy_chain.write_alpha_checkpoint(system.quantities, "pickled_data/N="+str(N+1)+"/quantities_with_reduced_alpha="+str(ind)+"_"+str(poss_alpha.index(1)))
-	xy_chain.write_alpha_checkpoint(system.integrated, "pickled_data/N="+str(N+1)+"/integration_with_reduced_alpha="+str(ind)+"_"+str(poss_alpha.index(1)))
+	two_qubit_fidelity = [fidelity(system.corr_state, time_evolved.ptrace([N-1,N])) for time_evolved in system.time_evo]
+	xy_chain.write_alpha_checkpoint(system.quantities, "pickled_data/N="+str(N+1)+"/quantities_with_reduced_alpha="+str(int((ind/100))%10)+str(int((ind/10))%10)+str(ind%10)+"_"+str(poss_alpha.index(1)))
+	xy_chain.write_alpha_checkpoint(system.integrated, "pickled_data/N="+str(N+1)+"/integration_with_reduced_alpha="+str(int((ind/100))%10)+str(int((ind/10))%10)+str(ind%10)+"_"+str(poss_alpha.index(1)))
+	xy_chain.write_alpha_checkpoint(two_qubit_fidelity, "pickled_data/N="+str(N+1)+"/two_qubit_fidelity_with_reduced_alpha="+str(int((ind/100))%10)+str(int((ind/10))%10)+str(ind%10)+"_"+str(poss_alpha.index(1)))
