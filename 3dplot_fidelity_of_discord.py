ff4f856fa1df4e5b1f43a6cf472f38e3b4949adf
Etienne Springer <esp8999@gmail.com>
2023-03-29 14:40:51 +0200

    QoL fix

--- 3dplot_fidelity_of_discord.py
+++ 3dplot_fidelity_of_discord.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import glob
+import pickle
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -13,21 +15,40 @@ cmap = mpl.cm.get_cmap('viridis')
 cmap_2 = mpl.cm.get_cmap('plasma')
 
 
-npzfile = np.load("fid_of_discord.npz")
-npzfile_both = np.load("new_two_fid_of_discord.npz")
+#npzfile = np.load("fid_of_discord.npz")
+#npzfile_both = np.load("new_two_fid_of_discord.npz")
+#
+#fid_of_t_local = npzfile['fid_of_t']
+#discord_local = npzfile['discord']
+#
+#fid_of_t_both_qubits = npzfile_both['fid_of_t']
+#discord_both_qubits = npzfile_both['discord']
+files = [file for file in glob.glob("pickled_data/N=6/new_two_*")]
+files.sort()
+files_2 = [file for file in glob.glob("pickled_data/N=6/metadata_for_*")]
+files_2.sort()
 
-fid_of_t_local = npzfile['fid_of_t']
-discord_local = npzfile['discord']
+fidelity = []
+discord = []
+t=[]
+for ind, file in enumerate(files):
+    with open(file, 'rb') as f:
+        #local_dict = pickle.load(f)
+        #print(type(local_dict))
+        fidelity.append(pickle.load(f))
+for file in files_2:
+    with open(file, 'rb') as f:
+        local_dict = pickle.load(f)
+        discord.append(local_dict['discord'])
+        t.append(local_dict['t'])
 
-fid_of_t_both_qubits = npzfile_both['fid_of_t']
-discord_both_qubits = npzfile_both['discord']
 
-fid_max = np.zeros_like(discord_both_qubits)
-t_of_fid_max = np.zeros_like(discord_both_qubits)
+fid_max = np.zeros_like(discord)
+t_of_fid_max = np.zeros_like(discord)
 
-for ind, fid in enumerate(fid_of_t_both_qubits.T):
-    fid_max[ind] = np.max(fid)
-    t_of_fid_max[ind] = np.argmax(fid)
+#for ind, fid in enumerate(fidelity.T):
+#    fid_max[ind] = np.max(fid)
+#    t_of_fid_max[ind] = np.argmax(fid)
 
 #print(t_of_fid_max)
 
@@ -39,9 +60,9 @@ fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
 #for ind, discord_value in enumerate(discord_local):
 #    ax.plot(t, fid_of_t_local[:,ind], '.', markersize=.5, zorder=ind, alpha=.6, zs=discord_value, zdir='y', color=cmap(discord_value/max(discord_local)))
     
-for ind, discord_value in enumerate(discord_both_qubits):
+for ind, discord_value in enumerate(discord):
     zs = discord_value
-    ax.plot(t, fid_of_t_both_qubits[:,ind], '.', markersize=.5, zorder=ind, zs=discord_value, zdir='y', color=cmap_2(discord_value/max(discord_both_qubits)))
+    ax.plot(t, fidelity[ind], '.', markersize=.5, zorder=ind, zs=discord_value, zdir='y', color=cmap_2(discord_value/max(discord)))
     #ax.scatter(t_of_fid_max[ind], fid_max[ind], zs, '.', color='black')
 
 ax.set_xlim(0,3.2)
