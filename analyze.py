b3eb883fe6430691cf108a9960b3103abe5a88f8
Etienne Springer <esp8999@gmail.com>
2023-03-29 16:50:41 +0200

    tidying up main.py and analyze.py

--- analyze.py
+++ analyze.py
@@ -23,79 +23,12 @@ files_2.sort()
 fidelity = []
 discord = []
 t=[]
-#
+
 for ind, file in enumerate(files):
     with open(file, 'rb') as f:
-        #local_dict = pickle.load(f)
-        #print(type(local_dict))
         fidelity.append(pickle.load(f))
 for file in files_2:
     with open(file, 'rb') as f:
         local_dict = pickle.load(f)
         discord.append(local_dict['discord'])
         t.append(local_dict['t'])
-
-
-#
-#
-#fid_of_t = np.zeros((3142,len(files)))
-#i_dot_sq_of_t = np.zeros((3141, len(files)))
-#for ind, file in enumerate(files):
-#    with open(file, 'rb') as f:
-#        local_dict = pickle.load(f)
-#        i_dot_sq_max.append(local_dict['i_dot_sq_max'])
-#        discord.append(local_dict['discord'])
-#        fid_of_t[:,ind] = local_dict
-#        #i_dot_sq_of_t[:,ind] = local_dict['i_dot_sq']
-print(len(fidelity))
-discord = np.array(discord)
-print(discord)
-
-
-#npzfile_both = np.load("two_fid_of_discord.npz")
-#
-#fid_of_t_both_qubits = npzfile_both['fid_of_t']
-#discord_both_qubits = npzfile_both['discord']
-
-
-#with open("new_two_fid_of_discord.npz", 'wb') as f:
-#    np.savez(f, fid_of_t=fid_of_t, discord=discord)
-#
-#fig, ax =plt.subplots(figsize=(8, 4.5))
-##ax.plot(discord, i_dot_sq_max/i_dot_sq_max[-1], '.', color=cmap(1/2), label = r'$\frac{\dot{I}_\mathrm{max}^2(D(\alpha))}{\dot{I}_\mathrm{max}^2(D(\alpha=0))}$')
-#t_stop = np.pi
-#dt = 1e-3
-#t = np.arange(0, t_stop, dt)
-###
-#fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
-#for ind, discord_value in enumerate(discord):
-#    ax.plot(t[:-1], i_dot_sq_of_t[:,ind], '.', markersize=.5, zs=discord_value, zdir='y', color=cmap(discord_value/max(discord)))
-#    new_zs = np.argmax(i_dot_sq_of_t[:,ind])*1e-3
-#    print(new_zs)
-#    ax.scatter(new_zs, i_dot_sq_max[ind], zdir='y',zs=discord_value, color='black')
-#ax.set_ylim(0,max(discord))
-#ax.set_ylabel(r"$D(\alpha)$")
-#ax.set_xlabel(r"$t$")
-##ax.set_ylabel(r"$\frac{\dot{I}_\mathrm{max}^2(D(\alpha))}{\dot{I}_\mathrm{max}^2(D(\alpha=0))}$")
-##ax.set_zlim(1/np.sqrt(2),1)
-#ax.view_init(elev=36., azim=70)
-##plt.savefig("plots/i_dot_sq_of_discord_3dplot_with_max.pdf")
-#plt.show()
-
-#fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(12,8))
-
-#for n in range(N+1):
-#    ax.plot(t, sz[n], label=r"$\expval{\sigma^z_{%d}}$"%(n+1), lw=1.5, color=cmap((n+1)/(N+2)))
-#ax.set_xlabel(r'Time')
-#ax.set_ylabel(r'$\expval{\sigma^z_i}$')
-#ax1.plot(t, fidelity_of_t, label=r"$F(\rho_\mathrm{corr, 12}, \rho_{56}^t)$", color=cmap(1/3))
-#ax2.plot(t, rel_ent_of_t, label=r'$\mathcal{S}(\rho_\mathrm{corr, 12} \mid\mid \rho_{56}^t)$', color=cmap(2/3))
-#ax2.set_yscale('log')
-#ax.axhline(0, color='grey', ls='--')
-#ax1.axhline(1, color='grey', ls='--')
-#ax2.plot(t[:-1], i_dot_sq_perf, color=cmap(2/3))
-#ax1.legend()
-#fig.legend(loc=8, ncol=4)
-#fig.tight_layout()
-#fig.subplots_adjust(bottom=.1)
-#plt.show()
--- main.py
+++ main.py
@@ -1,57 +1,19 @@
 import numpy as np
-from generate_states import generate_hamiltonian
 from qutip import *
-import matplotlib as mpl
-import matplotlib.pyplot as plt
 import xy_chain
-
-# for using tex formatting and font in plots
-
-plt.rcParams.update({"text.usetex": True,}) 
-plt.rcParams['text.latex.preamble'] = r'\usepackage[utf8]{inputenc}\usepackage[T1]{fontenc}\usepackage{lmodern}\inputencoding{utf8}\usepackage{amsmath}\usepackage{amssymb}\usepackage{dsfont}\usepackage{mathtools}\usepackage{physics}'
-mpl.rcParams['font.family'] = ['serif']
-
-cmap = mpl.cm.get_cmap('PuOr')
 
 t_stop = 2*np.pi
 dt = 1e-3
 t = np.arange(0, t_stop, dt)
 beta = [0, 0]
 N = 5
-Hint = generate_hamiltonian(N)
 
 poss_alpha = [i/12 for i in range(0,13)]
 
 for ind, reduced_alpha in enumerate(poss_alpha):
-	system = xy_chain.System(t, N, Hint, reduced_alpha, beta)
+	system = xy_chain.HeisenbergXY(t, N, reduced_alpha, beta)
 	two_qubit_fidelity = [fidelity(system.corr_state, time_evolved_system.ptrace([N,N-1])) for time_evolved_system in system.time_evo]
 	xy_chain.write_alpha_checkpoint(system.meta, "pickled_data/N="+str(N+1)+"/metadata_for_reduced_alpha="+str(int((ind/100))%10)+str(int((ind/10))%10)+str(ind%10)+"_"+str(poss_alpha.index(1)))
 	xy_chain.write_alpha_checkpoint(system.integrated, "pickled_data/N="+str(N+1)+"/integration_with_reduced_alpha="+str(int((ind/100))%10)+str(int((ind/10))%10)+str(ind%10)+"_"+str(poss_alpha.index(1)))
 	xy_chain.write_alpha_checkpoint(system.time_evo, "pickled_data/N="+str(N+1)+"/system_integrated_reduced_alpha="+str(int((ind/100))%10)+str(int((ind/10))%10)+str(ind%10)+"_"+str(poss_alpha.index(1)))
 	xy_chain.write_alpha_checkpoint(two_qubit_fidelity, "pickled_data/N="+str(N+1)+"/new_two_qubit_fidelity_with_reduced_alpha="+str(int((ind/100))%10)+str(int((ind/10))%10)+str(ind%10)+"_"+str(poss_alpha.index(1)))
-
-#xy_chain.write_alpha_checkpoint(system.quantities, "pickled_data/N="+str(N+1)+"/quantities_with_reduced_alpha="+str(int((ind/100))%10)+str(int((ind/10))%10)+str(ind%10)+"_"+str(poss_alpha.index(1)))
-"""
-Recycling bin:
-
-print(system.time_evo[-1].ptrace([N-1,N]))
-
-print(system.rho.eigenenergies())
-
-plt.figure(figsize=(12,5))
-for ind, sz in enumerate(system.integrated):
-	plt.plot(t, sz, label = str(ind), color = cmap((ind+1)/(N+2)))
-plt.legend()
-plt.show()
-
-prod_state = tensor(system.second_state, system.first_state)
-print(fidelity(prod_state-system.chi, system.time_evo[-1].ptrace([N-1,N])))
-
-system.single_state_fidelity()
-
-plt.figure(figsize=(14.5,8))
-plt.plot(t, system.quantities['fidelity'])
-plt.show()
-
-
-"""
