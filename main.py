0d9a8c220b5dd91ae49b5497fb0a189a63c0290f
Etienne Springer <esp8999@gmail.com>
2023-03-29 14:38:37 +0200

    clean up main.py and implement two-qubit fidelity

--- main.py
+++ main.py
@@ -13,33 +13,45 @@ mpl.rcParams['font.family'] = ['serif']
 
 cmap = mpl.cm.get_cmap('PuOr')
 
-t_stop = np.pi
+t_stop = 2*np.pi
 dt = 1e-3
 t = np.arange(0, t_stop, dt)
-beta = [0, .5]
+beta = [0, 0]
 N = 5
 Hint = generate_hamiltonian(N)
 
-poss_alpha = [0,.1,1]
+poss_alpha = [i/12 for i in range(0,13)]
 
 for ind, reduced_alpha in enumerate(poss_alpha):
 	system = xy_chain.System(t, N, Hint, reduced_alpha, beta)
-	#print(system.time_evo[-1].ptrace([N-1,N]))
-	print(system.rho.eigenenergies())
-	plt.figure(figsize=(12,5))
-	for ind, sz in enumerate(system.integrated):
-		plt.plot(t, sz, label = str(ind), color = cmap((ind+1)/(N+2)))
-	plt.legend()
-	plt.show()
-	#prod_state = tensor(system.second_state, system.first_state)
-	#print(fidelity(prod_state-system.chi, system.time_evo[-1].ptrace([N-1,N])))
-	#system.single_state_fidelity()
-	#plt.figure(figsize=(14.5,8))
-	#plt.plot(t, system.quantities['fidelity'])
-	#plt.show()
-	#two_qubit_fidelity = [fidelity(system.corr_state, time_evolved.ptrace([N,N-1])) for time_evolved in system.time_evo]
-	#xy_chain.write_alpha_checkpoint(system.quantities, "pickled_data/N="+str(N+1)+"/quantities_with_reduced_alpha="+str(int((ind/100))%10)+str(int((ind/10))%10)+str(ind%10)+"_"+str(poss_alpha.index(1)))
-	#xy_chain.write_alpha_checkpoint(system.integrated, "pickled_data/N="+str(N+1)+"/integration_with_reduced_alpha="+str(int((ind/100))%10)+str(int((ind/10))%10)+str(ind%10)+"_"+str(poss_alpha.index(1)))
-	#xy_chain.write_alpha_checkpoint(system.time_evo, )
-	#xy_chain.write_alpha_checkpoint(two_qubit_fidelity, "pickled_data/N="+str(N+1)+"/new_two_qubit_fidelity_with_reduced_alpha="+str(int((ind/100))%10)+str(int((ind/10))%10)+str(ind%10)+"_"+str(poss_alpha.index(1)))
+	two_qubit_fidelity = [fidelity(system.corr_state, time_evolved_system.ptrace([N,N-1])) for time_evolved_system in system.time_evo]
+	xy_chain.write_alpha_checkpoint(system.meta, "pickled_data/N="+str(N+1)+"/metadata_for_reduced_alpha="+str(int((ind/100))%10)+str(int((ind/10))%10)+str(ind%10)+"_"+str(poss_alpha.index(1)))
+	xy_chain.write_alpha_checkpoint(system.integrated, "pickled_data/N="+str(N+1)+"/integration_with_reduced_alpha="+str(int((ind/100))%10)+str(int((ind/10))%10)+str(ind%10)+"_"+str(poss_alpha.index(1)))
+	xy_chain.write_alpha_checkpoint(system.time_evo, "pickled_data/N="+str(N+1)+"/system_integrated_reduced_alpha="+str(int((ind/100))%10)+str(int((ind/10))%10)+str(ind%10)+"_"+str(poss_alpha.index(1)))
+	xy_chain.write_alpha_checkpoint(two_qubit_fidelity, "pickled_data/N="+str(N+1)+"/new_two_qubit_fidelity_with_reduced_alpha="+str(int((ind/100))%10)+str(int((ind/10))%10)+str(ind%10)+"_"+str(poss_alpha.index(1)))
 
+#xy_chain.write_alpha_checkpoint(system.quantities, "pickled_data/N="+str(N+1)+"/quantities_with_reduced_alpha="+str(int((ind/100))%10)+str(int((ind/10))%10)+str(ind%10)+"_"+str(poss_alpha.index(1)))
+"""
+Recycling bin:
+
+print(system.time_evo[-1].ptrace([N-1,N]))
+
+print(system.rho.eigenenergies())
+
+plt.figure(figsize=(12,5))
+for ind, sz in enumerate(system.integrated):
+	plt.plot(t, sz, label = str(ind), color = cmap((ind+1)/(N+2)))
+plt.legend()
+plt.show()
+
+prod_state = tensor(system.second_state, system.first_state)
+print(fidelity(prod_state-system.chi, system.time_evo[-1].ptrace([N-1,N])))
+
+system.single_state_fidelity()
+
+plt.figure(figsize=(14.5,8))
+plt.plot(t, system.quantities['fidelity'])
+plt.show()
+
+
+"""
