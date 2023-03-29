f391f5b82c2c15c3c7748334e62bfa6b6695b9b1
Etienne Springer <esp8999@gmail.com>
2023-03-29 16:48:18 +0200

    expand HeisenbergXY class

--- xy_chain.py
+++ xy_chain.py
@@ -2,13 +2,11 @@
 
 import os
 import pickle
-
 import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import prep_state
-from numpy import linalg as LA
+
+from scipy.signal import argrelmin
 from qutip import *
+
 from q_discord import quantum_discord
 
 # goal: system(10, 3, H, [a,b]) initializes a spinchain with given length (10), position of left qubit in corr state (3), an interaction H and two temperatures
@@ -20,183 +18,109 @@ from q_discord import quantum_discord
 # 
 # playing around with alpha and beta can give some arbitrary uncorrelated initial state at position 1
 
+"""
+There used to be two functions here.
 
-def integrate(N, H, psi0, tlist, solver):
+One returned a density matrix, where
+correlations could be at an arbitrary position. It can be found in a separate
+file named 'dumping_ground'.
 
-    si = qeye(2)
-    sx = sigmax()
-    sy = sigmay()
-    sz = sigmaz()
+The other one was a function which computed "long-range correlations",
+which turned out to not work due to the resulting density matrix no longer
+being positive. check previous versions for it if you must.
+"""
 
-    sx_list = []
-    sy_list = []
-    sz_list = []
-
-    for n in range(N+1):
-        op_list = [si for m in range(N+1)]
-
-        op_list[n] = sx
-        sx_list.append(tensor(op_list))
-
-        op_list[n] = sy
-        sy_list.append(tensor(op_list))
-
-        op_list[n] = sz
-        sz_list.append(tensor(op_list))
-
-    # construct the hamiltonian
-    #H = generate_states(N, interaction)
-
-    # energy splitting terms
-
-    # interaction terms
-    
-
-    # collapse operators
-    c_op_list = []
-
-
-    # evolve and calculate expectation values
-    if solver == "me":
-        result = mesolve(H, psi0, tlist, c_op_list, sz_list)
-    elif solver == "mc":
-        ntraj = 250 
-        result = mcsolve(H, psi0, tlist, c_op_list, sz_list, ntraj)
-    return result.expect
-
-
-def init_system(alpha, first_state, second_state, N):
-	astar = -alpha
-	rho_system = tensor(first_state, second_state)+ alpha * tensor(Qobj([[0,1],[0,0]]), Qobj([[0,0],[1,0]])) + astar * tensor(Qobj([[0,0],[1,0]]), Qobj([[0,1],[0,0]]))
-	for i in range(1,N):
-		rho_system = tensor(rho_system, Qobj([[0,0],[0,1]]))
-	return rho_system
-
-
-def system_arbitrary_pos_of_corr(alpha, first_state, prod_state, pos, N):
-	if pos == 1:
-		return init_system(alpha, prod_state.ptrace(0),prod_state.ptrace(1), N)
-	qubit_states = []
-	i = 1
-	astar = -alpha
-	corr_state = prod_state + alpha * tensor(Qobj([[0,1],[0,0]]), Qobj([[0,0],[1,0]])) + astar * tensor(Qobj([[0,0],[1,0]]), Qobj([[0,1],[0,0]]))
-	rho_system = first_state
-	while i < pos-1:
-		qubit_states.append(Qobj([[0,0],[0,1]]))
-		i+=1
-	qubit_states.append(corr_state)
-	i+=2
-	while i<=N:
-		qubit_states.append(Qobj([[0,0],[0,1]]))
-		i+=1
-	for rho in qubit_states:
-		rho_system = tensor(rho_system, rho)
-	return rho_system
-
-
-def init_system_arb_corr(first_state, nth_state, corr, alpha, N):
-    qubit_states = [Qobj([[0,0],[0,1]]) for i in range(N+1)]
-    qubit_states[corr[0]] = first_state
-    qubit_states[corr[1]] = nth_state
-    
-    rho_system = qubit_states[0]    # calculate product state of system
-
-    for state in qubit_states[1:]:
-        rho_system = tensor(rho_system, state)
-
-    # calculate longrange correlation term.
-    # this part of the code is proper garbage, but mathematically correct
-    # i need to think of something more efficient here
-    identities = [qeye(2) for i in range(corr[1]-corr[0])]  
-    sigma_p = [sigmap()]
-    sigma_m = [sigmam()]
-    sigma_p_0 = sigma_p.copy()
-    sigma_p_0.extend(identities)
-    sigma_m_0 = sigma_m.copy()
-    sigma_m_0.extend(identities)
-    sigma_p_1 = sigma_p.copy()
-    sigma_p_1.extend(identities)
-    sigma_p_1.reverse()
-    sigma_m_1 = sigma_m.copy()
-    sigma_m_1.extend(identities)
-    sigma_m_1.reverse()
-
-    chi = alpha * (tensor(sigma_p_0) * tensor(sigma_m_1) - tensor(sigma_m_0) * tensor(sigma_p_1))
-
-    chi_helper = [qeye(2) for i in range(N+1)]
-    chi_helper[corr[0]] = chi
-    del chi_helper[corr[0]+1:corr[1]+1]
-    total_chi = tensor(chi_helper)
-    #rho_system = rho_system + total_chi
-    return rho_system, total_chi
-
-
-def thermal_state(beta):
-    rho = (-beta * sigmaz()).expm()
-    Z = rho.tr()
-    return rho/Z
-
-
-class System: 
-    def __init__(self, t, N, H_given, alpha_reduced, beta: list) -> None: # , corr: list
+class HeisenbergXY: 
+    def __init__(self, t, N, alpha_reduced, beta: list) -> None: # , corr: list
         self.t = t                      # array of timesteps
         self.dt = self.t[1]             # length of timestep
         self.N = N                      # Number of qubits
-        self.H = H_given                # Hamiltonian
         self.beta = beta                # list of temperatures of the correlated qubits
+        self.alpha = alpha_reduced * self.get_max_alpha()
+
+        first_state  = (-beta[0]*sigmaz()).expm()/(2*np.cosh(beta[0]))
+        second_state = (-beta[1]*sigmaz()).expm()/(2*np.cosh(beta[1]))
         
-        self.meta = {"t":self.t, "dt":self.dt,"N":self.N, "beta":self.beta, "H":self.H}
-
-        self.first_state = thermal_state(self.beta[0])  # first state in the correlation pair
-        self.second_state = thermal_state(self.beta[1]) # second state in the correlation pair
-
-        self.alpha = alpha_reduced * 1.j / (4*np.cosh(self.beta[0])*np.cosh(self.beta[1]))
-        #self.prod_state = tensor(self.first_state, self.second_state) # product state
-        #if True: # a density matrix with longrange correlations is not positive semi-definite
-        self.prod_state = tensor(self.first_state, self.second_state) # product state
-        self.chi = self.alpha * tensor(Qobj([[0,1],[0,0]]), Qobj([[0,0],[1,0]])) - self.alpha * tensor(Qobj([[0,0],[1,0]]), Qobj([[0,1],[0,0]]))
-        self.corr_state = self.prod_state + self.chi
-        self.rho = init_system(self.alpha, self.first_state, self.second_state, self.N)
-        #else:    
-        #    self.corr = corr                # list of qubits to correlate. if None, defaults to [0,1], see if condition
-        #    rho, self.chi  = init_system_arb_corr(first_state=self.first_state, nth_state=self.second_state, corr=self.corr, alpha = self.alpha, N=self.N)
-        #    self.rho = rho + self.chi
-        #    self.prod_state = self.rho.ptrace(self.corr)
-        #    self.corr_state = self.prod_state + self.alpha * tensor(Qobj([[0,1],[0,0]]), Qobj([[0,0],[1,0]])) - self.alpha * tensor(Qobj([[0,0],[1,0]]), Qobj([[0,1],[0,0]]))
+        self.chi = self.alpha * tensor(sigmap(), sigmam()) - self.alpha * tensor(sigmam(), sigmap())
+        self.corr_state = tensor(first_state, second_state) + self.chi
+        
+        self.rho = self.init_system()
 
         self.discord = quantum_discord(self.corr_state)
+        self.calc_composite_ops()
+        self.compute_xy_hamiltonian()
 
-        self.time_evo = qutip.mesolve(self.H, self.rho, self.t, [], []).states
-        self.integrated = integrate(self.N, self.H, self.rho, self.t, "me")
+        self.time_evo = mesolve(self.H, self.rho, self.t, [], []).states
         self.quantities = {"fidelity":[], "rel_ent":[], "i_dot_sq":[], "i_dot_sq_max":[], "e_dot":[], "time_where_kld_isclose_0":[], "fid_max":[], "discord":self.discord}
-        self.meta = {"t":self.t, "dt":self.dt,"N":self.N, "beta":self.beta, "H":self.H, "discord":self.discord}
+        self.meta = {"t":self.t, "dt":self.dt,"N":self.N, "beta":beta, "H":self.H, "discord":self.discord}
 
-    
+
+    def calc_composite_ops(self):
+        self.sx_list = []
+        self.sy_list = []
+        self.sz_list = []
+        self.sp_list = []
+        self.sm_list = []
+
+        for n in range(self.N+1):
+            op_list = [qeye(2) for _ in range(self.N+1)]
+
+            op_list[n] = sigmax()
+            self.sx_list.append(tensor(op_list))
+
+            op_list[n] = sigmay()
+            self.sy_list.append(tensor(op_list))
+
+            op_list[n] = sigmaz()
+            self.sz_list.append(tensor(op_list))
+
+            op_list[n] = sigmap()
+            self.sp_list.append(tensor(op_list))
+
+            op_list[n] = sigmam()
+            self.sm_list.append(tensor(op_list))
+
+
+    def compute_xy_hamiltonian(self,  l=1., coupling=1/2):
+        hamiltonian = 0
+        for i in range(self.N):
+            hamiltonian += (coupling*l) / 2 * np.sqrt((i+1)*(self.N-i)) * (self.sx_list[i] * self.sx_list[i+1] + self.sy_list[i] * self.sy_list[i+1])
+            hamiltonian -= self.sz_list[i]
+        self.H = hamiltonian - self.sz_list[self.N]
+
+
+    def get_max_alpha(self):
+        return 1.j / (4*np.cosh(self.beta[0])*np.cosh(self.beta[1]))
+
+
+    def init_system(self):
+        rho_system = self.corr_state
+        for _ in range(1,self.N):
+            rho_system = tensor(rho_system, Qobj([[0,0],[0,1]]))
+        return rho_system
+
+
     def single_state_fidelity(self):
         self.quantities["fidelity"] = np.array([fidelity(self.first_state, time_evolved.ptrace(self.N)) for time_evolved in self.time_evo])
-        self._fidelity = self.quantities["fidelity"]
+        self.single_fidelity = self.quantities["fidelity"]
+        self.quantities["fid_max"] = np.amax(self.single_fidelity)
     
 
     def single_state_rel_ent(self):
         self.quantities["rel_ent"] = np.array([entropy_relative(self.first_state, time_evolved.ptrace(self.N)) for time_evolved in self.time_evo])
-        self._rel_ent = self.quantities["rel_ent"]
+        self.single_rel_ent = self.quantities["rel_ent"]
 
 
     def arrival_time(self):
         self.single_state_fidelity()
         self.single_state_rel_ent()
-        poss_t = np.where(np.isclose(self._rel_ent, 0, atol=1e-8), self.t, np.nan)
-        self.quantities["fid_max"] = np.amax(self._fidelity)
-        self.quantities["time_where_kld_isclose_0"] = poss_t
+        poss_t = argrelmin(self.single_rel_ent)
+        self.quantities["local minima in rel ent"] = poss_t
         
 
     def i_dot_sq(self):
         vn_ent = [entropy_vn(time_evolved.ptrace(self.N)) for time_evolved in self.time_evo]
         self.quantities["i_dot_sq"] = np.square(np.diff(vn_ent)/self.dt)
-    
-
-    def i_dot_sq_max(self):
-        self.i_dot_sq()
         self.quantities["i_dot_sq_max"] = np.max(self.quantities["i_dot_sq"])
 
     
@@ -204,10 +128,14 @@ class System:
         self.quantities["e_dot"] = [-1.j*(commutator(self.H, time_evolved).ptrace(self.N) * (sigmaz())).tr() for time_evolved in self.time_evo]
 
 
-    def calculate_quantities(self):
-        self.arrival_time()
-        self.i_dot_sq_max()
-        self.e_dot()
+    def integrate(self, solver="me"):
+        # evolve and calculate expectation values
+        if solver == "me":
+            result = mesolve(self.H, self.rho, self.t, [], self.sz_list)
+        elif solver == "mc":
+            ntraj = 250 
+            result = mcsolve(self.H, self.rho, self.t, [], self.sz_list, ntraj)
+        self.integrated = result.expect
 
 
 def write_alpha_checkpoint(system_quantities, path, overwrite=False):
