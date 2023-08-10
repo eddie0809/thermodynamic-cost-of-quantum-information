f7334076c7a4d4736994237274469e75cb05ec34
Etienne Springer <esp8999@gmail.com>
2023-08-10 11:14:26 +0200

    make xy_chain.py pep8 compliant

--- xy_chain.py
+++ xy_chain.py
@@ -14,26 +14,28 @@ class HeisenbergXY:
     """There used to be two more functions.
 
     One returned a system with initial density matrix, where
-    correlations could be at an arbitrary position. It can be found in a separate
-    file named 'dumping_ground'.
+    correlations could be at an arbitrary position.
+    It can be found in a separate file named 'dumping_ground'.
 
     The other one was a function which computed "long-range correlations",
     which turned out to not work due to the resulting density matrix no longer
     being positive. check previous versions for it if you must.
     """
-    def __init__(self, t, N, alpha_reduced, beta: list, corr:str, x_state:Qobj = None) -> None: # , corr: list
-        self.t = t                      # array of timesteps
-        self.dt = self.t[1]             # length of timestep
-        self.N = N                      # Number of qubits
-        self.beta = beta                # list of temperatures of the correlated qubits
+    def __init__(self, t, N, alpha_reduced,
+                 beta: list, corr: str, x_state: Qobj = None) -> None:
+        self.t = t             # array of timesteps
+        self.dt = self.t[1]    # length of timestep
+        self.N = N             # Number of qubits
+        self.beta = beta       # list of temperatures of the correlated qubits
         self.alpha = alpha_reduced * self.get_max_alpha()
 
-        if corr=="therm":
-            self.first_state  = (-beta[0]*sigmaz()).expm()/(2*np.cosh(beta[0]))
+        if corr == "therm":
+            self.first_state = (-beta[0]*sigmaz()).expm()/(2*np.cosh(beta[0]))
             second_state = (-beta[1]*sigmaz()).expm()/(2*np.cosh(beta[1]))
 
-            self.chi = self.alpha * tensor(sigmap(), sigmam()) - self.alpha * tensor(sigmam(), sigmap())
-            self.corr_state = tensor(self.first_state, second_state) + self.chi
+            self.chi = self.alpha * tensor(
+                 sigmap(), sigmam()) - self.alpha * tensor(sigmam(), sigmap())
+            self.corr_state = tensor(self.first_state, second_state)+self.chi
 
             self.rho = self.init_system()
 
@@ -41,14 +43,13 @@ class HeisenbergXY:
         elif corr == "X":
             self.corr_state = x_state
             self.rho = self.init_system()
-            #self.discord = quantum_discord(self.corr_state)
-        
+            # self.discord = quantum_discord(self.corr_state)
+
         self.calc_composite_ops()
         self.compute_xy_hamiltonian()
 
         self.time_evo = mesolve(self.H, self.rho, self.t, [], []).states
         self.quantities = {}
-
 
     def calc_composite_ops(self):
         self.sx_list = []
@@ -75,66 +76,67 @@ class HeisenbergXY:
             op_list[n] = sigmam()
             self.sm_list.append(tensor(op_list))
 
-
-    def compute_xy_hamiltonian(self,  l=1., coupling=1/2):
+    def compute_xy_hamiltonian(self,  lamb=1., coupling=1/2):
         hamiltonian = 0
         for i in range(self.N):
-            hamiltonian += (coupling*l) / 2 * np.sqrt((i+1)*(self.N-i)) * (self.sx_list[i] * self.sx_list[i+1] + self.sy_list[i] * self.sy_list[i+1])
+            hamiltonian += (coupling*lamb) / 2 * np.sqrt((i+1)*(self.N-i)) * (
+                 self.sx_list[i] * self.sx_list[i+1]
+                 + self.sy_list[i] * self.sy_list[i+1])
             hamiltonian -= self.sz_list[i]
         self.H = hamiltonian - self.sz_list[self.N]
-
 
     def get_max_alpha(self):
         return 1.j / (4*np.cosh(self.beta[0])*np.cosh(self.beta[1]))
 
-
     def init_system(self):
         rho_system = self.corr_state
-        for _ in range(1,self.N):
-            rho_system = tensor(rho_system, Qobj([[0,0],[0,1]]))
+        for _ in range(1, self.N):
+            rho_system = tensor(rho_system, Qobj([[0, 0], [0, 1]]))
         return rho_system
 
-
     def single_state_fidelity(self):
-        self.quantities["fidelity"] = np.array([fidelity(self.first_state, time_evolved.ptrace(self.N)) for time_evolved in self.time_evo])
+        self.quantities["fidelity"] = np.array(
+             [fidelity(self.first_state, time_evolved.ptrace(self.N))
+              for time_evolved in self.time_evo])
         self.single_fidelity = self.quantities["fidelity"]
         self.quantities["fid_max"] = np.amax(self.single_fidelity)
-    
 
     def single_state_rel_ent(self):
-        self.quantities["rel_ent"] = np.array([entropy_relative(self.first_state, time_evolved.ptrace(self.N)) for time_evolved in self.time_evo])
+        self.quantities["rel_ent"] = np.array(
+             [entropy_relative(self.first_state, time_evolved.ptrace(self.N))
+              for time_evolved in self.time_evo])
         self.single_rel_ent = self.quantities["rel_ent"]
-
 
     def arrival_time(self):
         self.single_state_fidelity()
         self.single_state_rel_ent()
         self.poss_t = argrelmin(self.single_rel_ent)
         self.quantities["local minima in rel ent"] = self.poss_t
-        
 
     def i_dot_sq(self):
-        vn_ent = [entropy_vn(time_evolved.ptrace(self.N)) for time_evolved in self.time_evo]
+        vn_ent = [entropy_vn(time_evolved.ptrace(self.N))
+                  for time_evolved in self.time_evo]
         self.quantities["i_dot_sq"] = np.square(np.diff(vn_ent)/self.dt)
         self.quantities["i_dot_sq_max"] = np.max(self.quantities["i_dot_sq"])
 
-    
-    def e_dot(self): # differentiating self.integrated does the same thing quicker (so far)
-        self.quantities["e_dot"] = [-1.j*(commutator(self.H, time_evolved).ptrace(self.N) * (sigmaz())).tr() for time_evolved in self.time_evo]
-
+    def e_dot(self):
+        # differentiating self.integrated does the same thing quicker (so far)
+        self.quantities["e_dot"] = [
+              -1.j*(commutator(self.H, evolved).ptrace(self.N)*sigmaz()).tr()
+              for evolved in self.time_evo]
 
     def integrate(self, solver="me"):
         # evolve and calculate expectation values
         if solver == "me":
             result = mesolve(self.H, self.rho, self.t, [], self.sz_list)
         elif solver == "mc":
-            ntraj = 250 
+            ntraj = 250
             result = mcsolve(self.H, self.rho, self.t, [], self.sz_list, ntraj)
         self.integrated = result.expect
 
 
 def write_alpha_checkpoint(system_quantities, path, overwrite=False):
-	if os.path.exists(path) and not overwrite:
-		raise RuntimeError("Data for this alpha already exist")
-	with open(path, 'wb') as fp:
-		pickle.dump(system_quantities, fp)
+    if os.path.exists(path) and not overwrite:
+        raise RuntimeError("Data for this alpha already exist")
+    with open(path, 'wb') as fp:
+        pickle.dump(system_quantities, fp)
