9e1045b02e6649a2dee9353d06e189ab47a5e547
Etienne Springer <esp8999@gmail.com>
2023-08-10 10:55:35 +0200

    make q_discord.py pep8 compliant

--- q_discord.py
+++ q_discord.py
@@ -3,16 +3,16 @@
 import numpy as np
 from qutip import *
 
+
 def quantum_discord(rho):
     x = np.zeros(3)*1j
-    V = np.zeros((3,3))*1j
-    sigma = np.array([sigmax(),sigmay(),sigmaz()])
-
+    V = np.zeros((3, 3))*1j
+    sigma = np.array([sigmax(), sigmay(), sigmaz()])
 
     for i, sig in enumerate(sigma):
-        x[i] = (rho*tensor(Qobj(sig),qeye(2))).tr() 
+        x[i] = (rho*tensor(Qobj(sig), qeye(2))).tr()
         for j, sigm in enumerate(sigma):
-            V[i,j] = (rho*tensor(Qobj(sig),Qobj(sigm))).tr()
+            V[i, j] = (rho*tensor(Qobj(sig), Qobj(sigm))).tr()
 
     x = Qobj(x)
     V = Qobj(V)
