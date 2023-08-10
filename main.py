7257c60be998917bf34b82aa6d0559d32d9759db
Etienne Springer <esp8999@gmail.com>
2023-08-10 10:27:00 +0200

    adapt main for X states

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import xy_chain
 import time
+from qutip import *
 
 t_stop = np.pi
 dt = 1e-3
@@ -8,17 +9,27 @@ t = np.arange(0, t_stop, dt)
 beta = [0, 0]
 N = 5
 
+x_state = Qobj([
+	[.4, 0, 0, 1.j*.04],
+	[0, .3, 1.j*.06, 0],
+    [0, -1.j*.06, .2, 0],
+    [-1.j*.04, 0, 0, .1]
+])
+print(x_state)
 poss_alpha = [i/100 for i in range(101)]
 
 arr_times = []
 discord = []
-for ind, reduced_alpha in enumerate(poss_alpha):
-	time_list = [time.time(), time.time()]
-	system = xy_chain.HeisenbergXY(t, N, reduced_alpha, beta)
-	system.arrival_time()
-	arr_times.append(system.__dict__['poss_t'][0])
-	discord.append(system.discord)
-	time_list[1] = time.time()
-	print(ind, np.abs(np.diff(time_list)))
-#xy_chain.write_alpha_checkpoint([arr_times, discord], "pickled_data/N="+str(N+1)+"/arr_time_for_reduced_alpha="+str(int((ind/100))%10)+str(int((ind/10))%10)+str(ind%10)+"_"+str(poss_alpha.index(1)))
-xy_chain.write_alpha_checkpoint([arr_times, discord], "pickled_data/N="+str(N+1)+"/arr_time_for_reduced_alpha")	
+system = xy_chain.HeisenbergXY(t, N, 0,beta, corr='X', x_state=x_state)
+system.arrival_time()
+print(system.quantities)
+#for ind, reduced_alpha in enumerate(poss_alpha):
+#	time_list = [time.time(), time.time()]
+#	system = xy_chain.HeisenbergXY(t, N, reduced_alpha, beta)
+#	system.arrival_time()
+#	arr_times.append(system.__dict__['poss_t'][0])
+#	discord.append(system.discord)
+#	time_list[1] = time.time()
+#	print(ind, np.abs(np.diff(time_list)))
+##xy_chain.write_alpha_checkpoint([arr_times, discord], "pickled_data/N="+str(N+1)+"/arr_time_for_reduced_alpha="+str(int((ind/100))%10)+str(int((ind/10))%10)+str(ind%10)+"_"+str(poss_alpha.index(1)))
+#xy_chain.write_alpha_checkpoint([arr_times, discord], "pickled_data/N="+str(N+1)+"/arr_time_for_reduced_alpha")	
